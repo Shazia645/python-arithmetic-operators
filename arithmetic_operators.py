a1f3b57848c022aab30bb027368e01982c6e49e3
Shazia <shaziabatool645@gmail.com>
2026-09-18 15:06:17 +0500

    Add arithmetic operators practice

---
+++ arithmetic_operators.py
@@ -0,0 +1,44 @@
+print("hello world")
+# print("you are  starting your python journey shazia now you have to stay consistance")
+print("my age is 20")
+print(23+22)
+price=34
+# print(price)
+print("his valu :", price)
+cost= 1000+455+900-500
+print("here is the exact cost of our company which i gona sell :",cost)
+Name= "Shazia batool"
+print(type(Name))
+print(type(cost))
+print(type(price))
+# arithematic opearaters
+a=2
+b=5
+sum=a+b
+print(a+b)
+print(a-b)
+print(a%b) #modulo remender findout kr ka data hai print(a*b)
+print(a/b)
+print(not(a<b))# not false kar da ga
+
+s=int("6")# type casting
+k=6
+sum=s+k
+print(type(s))
+print(sum)
+name=(input("please enter your name :",))
+print("welcom:",name)
+
+
+val=input("please enter some value:",)
+print(type(val),val)
+# age=input("enter your age:",)
+# marks=input("enter your marks:",)
+
+
+ab=int(input("please enter first num:",))
+bc=int(input("second num:",))
+print(ab>=bc)
+t=float(input("please enter first num:",))
+v=float(input("second num:",))
+print("average:",(t+v) /3)
